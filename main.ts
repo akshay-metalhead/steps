6666578ca2f7d86431b5833df5120f8e73210ee7
akshay-metalhead <74028267+akshay-metalhead@users.noreply.github.com>
2020-11-11 18:34:11 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    steps = 0
+})
 input.onGesture(Gesture.Shake, function () {
     steps += 1
 })
 let steps = 0
+steps = 0
 led.stopAnimation()
 basic.forever(function () {
     basic.showNumber(steps)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+    }
 })
